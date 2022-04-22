a76113aa5acbf411b9ebeba2ac8148498c351e2e
Alex Psitos <apsitos08@gmail.com>
2022-04-22 09:47:42 -0400

    Feat: create route, controller, and controller action for Bulk Discount

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   resources :merchant, only: [:show] do
     resources :dashboard, only: [:index]
+    resources :bulk_discounts
     resources :items, except: [:destroy]
     resources :item_status, only: [:update]
     resources :invoices, only: [:index, :show, :update]
