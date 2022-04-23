acf234a7070fc7e7d407664f22d5b06dd3e016c6
Alex Psitos <apsitos08@gmail.com>
2022-04-23 16:10:04 -0400

    Feat: model method match invoice item to bulk discount passes spec

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -17,6 +17,19 @@ class InvoiceItem < ApplicationRecord
     Invoice.order(created_at: :asc).find(invoice_ids)
   end
 
+  # def find_discount(id)
+  #   invoice_item = InvoiceItem.find(id)
+  #   bulk_discounts = BulkDiscount.order(:threshold)
+  #   discount = nil
+  #   bulk_discounts.each do |bd|
+  #     if invoice_item.quantity >= bd.threshold
+  #       discount = d
+  #     end
+  #   end
+  #   discount
+  # end
+  #above method has a lot of Ruby, but does the same thing as the below method:
+
   def match_invoice_item_to_discount
     bulk_discounts.where('bulk_discounts.threshold <= ?', quantity)
                   .select('bulk_discounts.*')
